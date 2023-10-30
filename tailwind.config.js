8d4b0cf35c87cadc2cd78eac3173af41efacbe95
Tibor <bratoblog@gmail.com>
2023-10-30 15:07:02 +0100

    add partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./app/**/*.liquid"],
   theme: {
-    extend: {},
+    extend: {
+      'blue_button': {
+        cursor: 'pointer',
+        backgroundColor: 'rgb(59 130 246)',
+        color: 'white',
+        fontWeight: 700,
+        padding: '0.5rem 1rem',
+        borderRadius: '0.25rem',
+      },
+    },
   },
   plugins: [],
 }
